3fe66116dedf0f576772ec5efcef76ec6b92d7e7
win-dish <jakob.windisch@student.uibk.ac.at>
2024-11-20 14:38:47 +0100

    working version (no checkpointing)

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -7,6 +7,11 @@ PY_ARGS=${@:3}
 PORT=${PORT:-29500}
 
 WORK_DIR=$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/
+CHECKPOINT_DIR="${WORK_DIR}latest.pth"
 
-$PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+echo "$CHECKPOINT_DIR"
+
+torchrun --nproc_per_node=$GPUS --master_port=$PORT \
     tools/train.py $CFG --work_dir $WORK_DIR --seed 0 --launcher pytorch ${PY_ARGS}
+
+#./tools/dist_train.sh configs/selfsup/densecl/densecl_coco_800ep.py 1 
